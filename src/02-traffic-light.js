9f2909a1b73937c739b5a48a472276ed3d2a0891
Abhijeet <abhijeetsinha2904@gmail.com>
2026-02-10 23:57:17 +0530

    Completed 2nd que

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -23,4 +23,18 @@
  */
 export function getTrafficAction(color) {
   // Your code here
+  const lower = color.toLowerCase();
+  switch(lower){
+    case 'green':
+      return "GO"
+      
+    case 'yellow':
+      return "SLOW DOWN"
+    case 'red':
+      return "STOP"
+    case 'flashing red':
+      return "STOP AND PROCEED WITH CAUTION"
+    default:
+      return "INVALID SIGNAL"
+  }
 }
